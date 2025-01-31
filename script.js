c38963d5dd1fb3730df6294933305e18f7b132ad
Sofi Brugo <brugosofi@gmail.com>
2025-01-31 19:04:54 -0300

    feat: add design feature

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 //variables
-const tarjetas = document.querySelectorAll('.about-containers .details-container');
-const titulos = document.querySelectorAll('div h3 a');
+const tarjetas = document.querySelectorAll(
+  ".about-containers .details-container"
+);
+const titulos = document.querySelectorAll("div h3 a");
 
-
-
-
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open")
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
+//function used to show description based on the clicked title
+function showArticle(title) {
+  const description = document.getElementById(title);
+  description.classList.toggle("show");
+  description.classList.toggle("hidden");
+}
