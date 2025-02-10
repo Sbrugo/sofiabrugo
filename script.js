a007f4fb5c91abf79a787241420ec526f331eed0
brugos <brugos@jefatura.gob.ar>
2025-02-10 15:25:15 -0300

    feat: add new description

--- script.js
+++ script.js
@@ -11,34 +11,10 @@ function toggleMenu() {
 }
 
 //function used to show description based on the clicked title
-function showArticle(subject) {
-  const description = document.querySelector(".description-container");
+function showArticle(title) {
+  const description = document.getElementById(title);
   description.classList.toggle("show");
   description.classList.toggle("hidden");
-
-  if (subject === "htmlcss") {
-    description.innerHTML = `
-            <h2>HTML & CSS</h2>
-            <ul>
-              <li>Responsive web pages</li>
-              <li>Structured content</li>
-              <li>DOM Manipulation</li>
-              <li>Styled CSS</li>
-            </ul>
-            <p>
-              By far what I have more experience on in frontend web development.
-            </p>
-            <h4>Examples:</h4>
-            <a
-              href="https://sbrugo.github.io/HTMLCSS/Responsive-web-page/index.html"
-              >Landing Page</a
-            >
-            <p>
-              I worked translating designs into markup language and layout
-              ensuring to reflected the visual and functional structure and
-              implementing it on frontend frameworks.
-            </p>`;
-  }
 }
 function openPanel() {
   const body = document.querySelector(".panel-body");
