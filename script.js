5a3fa3b5e3d473ea5ed40b697142d8376e7d21de
brugos <brugos@jefatura.gob.ar>
2025-02-12 12:31:34 -0300

    feat: add java

--- script.js
+++ script.js
@@ -12,10 +12,20 @@ function toggleMenu() {
 
 //function used to show description based on the clicked title
 function showArticle(title) {
+  const allSections = document.querySelectorAll('.description-container');
+
+  allSections.forEach((section) => {
+    section.parentElement.classList.add('hidden');
+    section.parentElement.classList.remove('show');
+  });
+
   const description = document.getElementById(title);
-  description.classList.toggle("show");
-  description.classList.toggle("hidden");
+  description.classList.toggle('show');
+  description.classList.toggle('hidden');
 }
-function openPanel() {
-  const body = document.querySelector(".panel-body");
+
+function closeSection(title) {
+  const section = document.getElementById(title);
+  section.classList.add('hidden');
+  section.classList.remove('show');
 }
